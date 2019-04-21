01231d9707e5b82d07da5358b11724dbe82109f9
hurryuphurryup <m13405771672@163.com>
2019-04-21 08:55:21 +0800

    springBoot_v1

---
+++ src/main/java/com/App.java
@@ -0,0 +1,20 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//@EnableAutoConfiguration
+@SpringBootApplication
+public class App {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(App.class, args);
+    }
+}
